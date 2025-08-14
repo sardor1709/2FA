6dc0a8eb2922ef5d31cbfb2101511db4c4c34855
S <131908589+sardor1709@users.noreply.github.com>
2025-08-14 16:50:38 +0500

    Add files via upload

---
+++ src/settings.py
@@ -0,0 +1,30 @@
+from decouple import config
+
+class Config:
+    SECRET_KEY = config("SECRET_KEY", default="guess-me")
+    APP_NAME = config("APP_NAME", default="MyFlaskApp")
+    SQLALCHEMY_DATABASE_URI = config("DATABASE_URL", default="sqlite:///mydatabase.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BCRYPT_LOG_ROUNDS = 13
+    WTF_CSRF_ENABLED = True
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    WTF_CSRF_ENABLED = False
+    DEBUG_TB_ENABLED = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///testdb.sqlite"
+    BCRYPT_LOG_ROUNDS = 1
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    DEBUG_TB_ENABLED = False
